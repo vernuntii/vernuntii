2b6310204707ebe8709670417ea7629971092651
teneko <giteroneko@gmail.com>
2022-05-02 00:38:40 +0100

    test: moved SemVer tests to SemVer-project

--- test/Vernuntii.Git.Test/Git/CommitVersionFinderTest.cs
+++ test/Vernuntii.Git.Test/Git/CommitVersionFinderTest.cs
@@ -11,14 +11,13 @@ namespace Vernuntii.Git
             using var repository = new TempoararyRepository(LoggerFactory.CreateLogger<TempoararyRepository>());
             repository.CommitEmpty();
             repository.TagLightweight("0.0.1");
-            repository.TagLightweight("0.0.1-alpha");
-            repository.TagLightweight("0.0.1-beta");
+            repository.TagLightweight("0.1.0");
 
             var version = new CommitVersionFinder(new CommitVersionFinderOptions(), repository, repository, LoggerFactory.CreateLogger<CommitVersionFinder>())
                 .FindCommitVersion(new CommitVersionFindingOptions());
 
             Assert.NotNull(version);
-            Assert.Equal(SemanticVersion.OnePatch, version!, SemanticVersionComparer.VersionReleaseBuild);
+            Assert.Equal(SemanticVersion.OneMinor, version!, SemanticVersionComparer.VersionReleaseBuild);
         }
     }
 }
