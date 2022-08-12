291440c5670efde26590e463e873f6ba9e5e5707
teneko <giteroneko@gmail.com>
2022-08-12 23:31:19 +0100

    fix: removed test code

--- src/Vernuntii.Console.Generic/ConsoleProgram.cs
+++ src/Vernuntii.Console.Generic/ConsoleProgram.cs
@@ -1,30 +1,7 @@
 ﻿using Vernuntii.Console;
 
-var i = new B() as A as I;
-i.Call();
-
 await using var runner = new VernuntiiRunner() {
     ConsoleArgs = args
 };
 
 return await runner.RunConsoleAsync();
-
-interface I
-{
-    void Call();
-}
-
-public class A : I
-{
-    void I.Call() => Console.WriteLine("A");
-}
-
-public class B : A, I
-{
-    void I.Call() {
-        Console.WriteLine("B");
-        A test = this;
-        var test2 = (I)test;
-        test2.Call();
-    }
-}
