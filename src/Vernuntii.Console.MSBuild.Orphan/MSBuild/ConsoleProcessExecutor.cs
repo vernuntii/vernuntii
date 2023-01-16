cb4339c6208ceab26b2f3486e7194ab6733795d6
teneko <giteroneko@gmail.com>
2023-01-16 06:38:33 +0000

    fix: increased daemon-timeout and implemented fallback

--- src/Vernuntii.Console.MSBuild.Orphan/MSBuild/ConsoleProcessExecutor.cs
+++ src/Vernuntii.Console.MSBuild.Orphan/MSBuild/ConsoleProcessExecutor.cs
@@ -15,11 +15,7 @@ namespace Vernuntii.Console.MSBuild
         private static readonly Dictionary<ConsoleProcessExecutionArguments, VernuntiiDaemon> s_consoleExecutableAssociatedDaemonDictionary;
         private static bool s_areDaemonsTerminated;
 
-        static ConsoleProcessExecutor()
-        {
-            s_consoleExecutableAssociatedDaemonDictionary = new();
-            //TerminateDaemons();
-        }
+        static ConsoleProcessExecutor() => s_consoleExecutableAssociatedDaemonDictionary = new();
 
         private static VernuntiiDaemon CreateDaemon(ConsoleProcessExecutionArguments executionArguments, TaskLoggingHelper logger)
         {
@@ -29,7 +25,7 @@ namespace Vernuntii.Console.MSBuild
                 " --daemon" +
                 $" {sendingPipe.GetClientHandleAsString()}" +
                 $" {receivingPipe.GetClientHandleAsString()}" +
-                " --daemon-timeout 60";
+                " --daemon-timeout 300";
             var startInfo = new ConsoleProcessStartInfo(executionArguments.ConsoleExecutablePath, processArguments);
             var loggerHolder = new TaskLoggingHelperHolder() { Logger = logger };
 
@@ -59,16 +55,18 @@ namespace Vernuntii.Console.MSBuild
             }
         }
 
-        //private static void TerminateDaemons()
-        //{
-        //    lock (s_consoleExecutableAssociatedDaemonDictionary) {
-        //        foreach (var daemon in s_consoleExecutableAssociatedDaemonDictionary.Values) {
-        //            daemon.Dispose();
-        //        }
-
-        //        s_areDaemonsTerminated = true;
-        //    }
-        //}
+        private static void TerminateDaemon(ConsoleProcessExecutionArguments executionArguments)
+        {
+            lock (s_consoleExecutableAssociatedDaemonDictionary) {
+                if (s_consoleExecutableAssociatedDaemonDictionary.TryGetValue(executionArguments, out var daemon)) {
+                    try {
+                        daemon.Dispose();
+                    } finally {
+                        s_consoleExecutableAssociatedDaemonDictionary.Remove(executionArguments);
+                    }
+                }
+            }
+        }
 
         private readonly TaskLoggingHelper _logger;
 
@@ -78,7 +76,7 @@ namespace Vernuntii.Console.MSBuild
         }
 
         [SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>")]
-        public GitVersionPresentation Execute(ConsoleProcessExecutionArguments arguments)
+        public GitVersionPresentation Execute(ConsoleProcessExecutionArguments arguments, bool isNextTry)
         {
             var daemon = GetOrCreateDaemon(arguments, _logger);
             using var logProcessExit = daemon.Process.Exited.Register(() => _logger.LogError($"The {nameof(Vernuntii)} daemon process exited unexpectely"));
@@ -87,13 +85,22 @@ namespace Vernuntii.Console.MSBuild
             NextVersionDaemonProtocolMessageType nextVersionMessageType;
 
             lock (daemon.ProtocolSynchronizationLock) {
-                daemon.LoggetHolder.Logger = _logger;
-                var sendingPipe = daemon.SendingPipe;
-                sendingPipe.WriteByte(NextVersionDaemonProtocolDefaults.Delimiter);
-                sendingPipe.Flush();
+                try {
+                    daemon.LoggetHolder.Logger = _logger;
+                    var sendingPipe = daemon.SendingPipe;
+                    sendingPipe.WriteByte(NextVersionDaemonProtocolDefaults.Delimiter);
+                    sendingPipe.Flush();
 
-                nextVersionMessage = new MemoryStream();
-                nextVersionMessageType = daemon.NextVersionPipeReader.ReadNextVersionAsync(nextVersionMessage).GetAwaiter().GetResult();
+                    nextVersionMessage = new MemoryStream();
+                    nextVersionMessageType = daemon.NextVersionPipeReader.ReadNextVersionAsync(nextVersionMessage).GetAwaiter().GetResult();
+                } catch {
+                    if (!isNextTry && daemon.Process.IsCompleted) {
+                        TerminateDaemon(arguments);
+                        return Execute(arguments, isNextTry: true);
+                    }
+
+                    throw;
+                }
             }
 
             daemon.NextVersionPipeReader.ValidateNextVersion(nextVersionMessageType, nextVersionMessage.GetBuffer);
@@ -105,6 +112,10 @@ namespace Vernuntii.Console.MSBuild
                 throw new NextVersionApiException($"The {nameof(Vernuntii)} daemon process could not produce a valid deserializable JSON object: {error}");
             }
         }
+
+        [SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>")]
+        public GitVersionPresentation Execute(ConsoleProcessExecutionArguments arguments) =>
+            Execute(arguments, isNextTry: false);
 
         private sealed class TaskLoggingHelperHolder
         {
