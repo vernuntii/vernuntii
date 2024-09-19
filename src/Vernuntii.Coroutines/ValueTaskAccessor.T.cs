4b7652f7ab4655f961756099b5ac5f4669535e0b
teneko <giteroneko@gmail.com>
2024-09-19 04:21:08 +0100

    Added ValueTaskAccessor structs

---
+++ src/Vernuntii.Coroutines/ValueTaskAccessor.T.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Vernuntii.Coroutines;
+
+[StructLayout(LayoutKind.Auto)]
+internal struct ValueTaskAccessor<TResult>
+{
+    internal object? _obj;
+    [AllowNull] internal readonly TResult _result;
+    internal short _token;
+    internal bool _continueOnCapturedContext;
+}
---
+++ src/Vernuntii.Coroutines/ValueTaskAccessor.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Vernuntii.Coroutines;
+
+[StructLayout(LayoutKind.Auto)]
+internal struct ValueTaskAccessor
+{
+    internal object? _obj;
+    internal short _token;
+    internal bool _continueOnCapturedContext;
+}
